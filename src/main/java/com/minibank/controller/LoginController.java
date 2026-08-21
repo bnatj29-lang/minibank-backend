97aac22d051fab5c6ebca57619277c73ae28d921
Bruna da Silva <b.natj29@gmail.com>
2026-08-20 22:54:45 -0300

    Ajustado para funcionar integrado com o front

--- src/main/java/com/minibank/controller/LoginController.java
+++ src/main/java/com/minibank/controller/LoginController.java
@@ -6,13 +6,11 @@ import com.minibank.service.LoginService;
 import com.minibank.service.PainelService;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/autenticar")
 @RestController
+@CrossOrigin(origins = "http://localhost:5173")
 public class LoginController {
 
     private final PainelService painelService;
