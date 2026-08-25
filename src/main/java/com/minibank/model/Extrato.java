f30d106a2f0b5299af4d38e1fff732eb76a24524
Rafaela Fernandes <rafaela63466666@alunos.sc.senac.br>
2026-08-25 16:25:26 -0300

    classe extrato, getters and setters

---
+++ src/main/java/com/minibank/model/Extrato.java
@@ -0,0 +1,68 @@
+package com.minibank.model;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class Extrato {
+    private Long id;
+    private Long criancaId;
+    private String tipo;
+    private BigDecimal valor;
+    private String descricao;
+    private LocalDate data;
+
+    public Long getId() {
+        return id; //DEVOLVE O VALOR GUARDADO NO ATRIBUTO ID PARA A CLASSE PEDINDO
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    } //this.id É O ATRIBUTO DA CLASSE e id É O PARAMETRO RECEBIDO PELO METODO
+      //entao o atributo id recebe o id;
+
+    public Long getCriancaId() {
+        return criancaId;
+    }
+
+    public void setCriancaId(Long criancaId) {
+        this.criancaId = criancaId;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo){
+        this.tipo = tipo;
+    }
+
+    public BigDecimal getValor(){
+        return valor;
+    }
+
+    public void setValor(BigDecimal valor) {
+        this.valor = valor;
+    }
+
+    public String getDescricao(){
+        return descricao;
+    }
+    public void setDescricao(String descricao){
+        this.descricao = descricao;
+    }
+
+    public LocalDate getData(){
+        return data;
+    }
+    public void setData(LocalDate data){
+        this.data = data;
+    }
+
+    public Extrato(Long id, Long criancaId, String tipo, BigDecimal valor, String descricao, LocalDate data){
+        this.id = id;
+        this.criancaId = criancaId;
+        this.tipo = tipo;
+        this.valor = valor;
+        this.descricao = descricao;
+        this.data = data;
+    }
+}
