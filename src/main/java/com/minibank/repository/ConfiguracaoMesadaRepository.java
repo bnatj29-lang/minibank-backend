f33f6190f804b79411e1d14e10ae3ae244e4b158
bruna63466756 <b.natj29@gmail.com>
2026-08-26 16:50:06 -0300

    Atualizando ConfiguracaoMesadaRepository

--- src/main/java/com/minibank/repository/ConfiguracaoMesadaRepository.java
+++ src/main/java/com/minibank/repository/ConfiguracaoMesadaRepository.java
@@ -4,6 +4,8 @@ import com.minibank.model.ConfiguracaoMesada;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import java.sql.ResultSet;
+
 @Repository
 public class ConfiguracaoMesadaRepository {
 
@@ -38,6 +40,51 @@ public class ConfiguracaoMesadaRepository {
                 configuracao.getValorFaixaBaixa(),
                 configuracao.getValorFaixaIntermediaria(),
                 configuracao.getValorFaixaMaxima()
+        );
+    }
+
+    public ConfiguracaoMesada buscarCrianca(Long criancaId) {
+
+        String sql = "SELECT * FROM configuracao_mesada WHERE crianca_id = ?";
+
+        return jdbcTemplate.queryForObject(
+                sql,
+                (rs, rowNum) -> new ConfiguracaoMesada(
+                        rs.getLong("id"),
+                        rs.getLong("crianca_id"),
+                        rs.getBigDecimal("valor_base"),
+                        rs.getBigDecimal("nota_minima_intermediaria"),
+                        rs.getBigDecimal("nota_minima_maxima"),
+                        rs.getBigDecimal("valor_faixa_baixa"),
+                        rs.getBigDecimal("valor_faixa_intermediaria"),
+                        rs.getBigDecimal("valor_faixa_maxima")
+                ),
+                criancaId
+        );
+    }
+
+    public void atualizar(ConfiguracaoMesada configuracao) {
+
+        String sql = """
+            UPDATE configuracao_mesada
+            SET valor_base = ?,
+                nota_minima_intermediaria = ?,
+                nota_minima_maxima = ?,
+                valor_faixa_baixa = ?,
+                valor_faixa_intermediaria = ?,
+                valor_faixa_maxima = ?
+            WHERE crianca_id = ?
+            """;
+
+        jdbcTemplate.update(
+                sql,
+                configuracao.getValorBase(),
+                configuracao.getNotaMinimaIntermediaria(),
+                configuracao.getNotaMinimaMaxima(),
+                configuracao.getValorFaixaBaixa(),
+                configuracao.getValorFaixaIntermediaria(),
+                configuracao.getValorFaixaMaxima(),
+                configuracao.getCriancaId()
         );
     }
 }
