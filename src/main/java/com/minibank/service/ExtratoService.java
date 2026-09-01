ac345388f373041b6f2e4f5be41af7369e44ebc9
Rafaela Fernandes <rafaela63466666@alunos.sc.senac.br>
2026-09-01 15:11:15 -0300

    ExtratoService completo

--- src/main/java/com/minibank/service/ExtratoService.java
+++ src/main/java/com/minibank/service/ExtratoService.java
@@ -24,8 +24,31 @@ public class ExtratoService {
   //METODO QUE REGISTRA A MOVIMENTACAO
     //recebe objeto dto contendo os dados enviados pelo front
   public void registrar(RegistrarExtratoRequestDTO request){
-      Extrato extrato = new Extrato();
 
+      //1- valor maior que zero?
+
+      if(request.getValor().compareTo(BigDecimal.ZERO) <= 0){
+          throw new IllegalArgumentException("o valor deve ser maior que zero.");
+      }
+
+      //2- o tipo ENTRADA OU RETIRADA
+
+      if(!request.getTipo().equals("ENTRADA") &&
+         !request.getTipo().equals("RETIRADA")){
+          throw new IllegalArgumentException("Tipo de movimentação inválido.");
+      }
+
+      //3- verificar saldo se for retirada
+      if(request.getTipo().equals("RETIRADA")) {
+          BigDecimal saldo = calcularSaldo(request.getCriancaId());
+
+          if(request.getValor().compareTo(saldo) > 0){
+              throw new IllegalArgumentException("Saldo insuficiente."); //interropcao do metodo
+          }
+      }
+
+      //4- depois de todas as validacoes, criacao do objeto Extrato
+      Extrato extrato = new Extrato();
       extrato.setCriancaId(request.getCriancaId());
       //Pegamos o criancaId que está dentro do DTO e colocamos dentro do objeto Extrato.
       extrato.setTipo(request.getTipo()); //(pegamos o tipo entrada/retirada)
@@ -33,7 +56,8 @@ public class ExtratoService {
       extrato.setDescricao(request.getDescricao()); //
       extrato.setData(LocalDate.now());
 
-      // O Repository é quem vai executar o INSERT no MySQL.
+
+      // O Repository é quem vai executar o INSERT no MySQL. - SALVA NO BANCO
       extratoRepository.salvar(extrato);
   }
 
@@ -56,4 +80,7 @@ public class ExtratoService {
       }
       return saldo; //saldo é um resultado temporario calculado a partir dos valores
   }
+
+
+
 }
