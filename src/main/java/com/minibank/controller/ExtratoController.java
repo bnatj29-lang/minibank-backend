35ca6b6a7cf71eef5e542d8682f56659d37de621
Rafaela Fernandes <rafaela63466666@alunos.sc.senac.br>
2026-09-01 15:19:20 -0300

    ExtratoController completo

--- src/main/java/com/minibank/controller/ExtratoController.java
+++ src/main/java/com/minibank/controller/ExtratoController.java
@@ -1,29 +1,42 @@
 package com.minibank.controller;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestBody;
-import com.minibank.dto.RegistrarExtratoRequestDTO;  //importa o dto (RECEBERA OS DADOS DO FRONT)
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.minibank.dto.RegistrarExtratoRequestDTO;
+import com.minibank.model.Extrato;
 import com.minibank.service.ExtratoService;
+
+import java.util.List;
 
 
 @RestController //essa anotacap diz ao spring que a classe é um controller - recebe requisicoes http
 public class ExtratoController {
 
-    //criamos um atributo para guardar o service
+    //criamos um atributo para guardar o Service
     private final ExtratoService extratoService;
 
     public ExtratoController(ExtratoService extratoService) {
         this.extratoService = extratoService; //guarda o service dentro do atributo
     }
 
-    //METODO EXECUTADO QND RECEBEMOS UMA REQUISICAO HTTP POST
+
+    //==========
+    //ENDPOINTS//
+    //==========
+
+    //METODO EXECUTADO QND RECEBEMOS UMA REQUISICAO HTTP POST - registrar movimentacao
     @PostMapping("/extrato")
     public void registrar(@RequestBody RegistrarExtratoRequestDTO request) {
         extratoService.registrar(request); //chamando metodo do service
         //passa para ele o dto recebido
     }
 
-
+  @GetMapping("/extrato{criancaId}") //consulta o extrato
+    public List<Extrato> consultar(@PathVariable Long criancaId){
+        return extratoService.consultar(criancaId);
+  }
 
 }
