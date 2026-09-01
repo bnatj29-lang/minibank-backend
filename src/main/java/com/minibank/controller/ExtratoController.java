de24be8c8597483399b52d4993643ea6a0f0542a
Rafaela Fernandes <rafaela63466666@alunos.sc.senac.br>
2026-09-01 14:29:27 -0300

    extrato service e controller update

--- src/main/java/com/minibank/controller/ExtratoController.java
+++ src/main/java/com/minibank/controller/ExtratoController.java
@@ -1,14 +1,29 @@
 package com.minibank.controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestBody;
-import com.minibank.dto.RegistrarExtratoRequestDTO;
+import com.minibank.dto.RegistrarExtratoRequestDTO;  //importa o dto (RECEBERA OS DADOS DO FRONT)
+import com.minibank.service.ExtratoService;
 
-@RestController
+
+@RestController //essa anotacap diz ao spring que a classe é um controller - recebe requisicoes http
 public class ExtratoController {
 
-    @PostMapping
-    public void registrar(@RequestBody RegistrarExtratoRequestDTO request){
+    //criamos um atributo para guardar o service
+    private final ExtratoService extratoService;
 
+    public ExtratoController(ExtratoService extratoService) {
+        this.extratoService = extratoService; //guarda o service dentro do atributo
     }
+
+    //METODO EXECUTADO QND RECEBEMOS UMA REQUISICAO HTTP POST
+    @PostMapping("/extrato")
+    public void registrar(@RequestBody RegistrarExtratoRequestDTO request) {
+        extratoService.registrar(request); //chamando metodo do service
+        //passa para ele o dto recebido
+    }
+
+
+
 }
--- src/main/java/com/minibank/service/ExtratoService.java
+++ src/main/java/com/minibank/service/ExtratoService.java
@@ -1,13 +1,36 @@
 package com.minibank.service;
+import com.minibank.dto.RegistrarExtratoRequestDTO;
+import com.minibank.model.Extrato;
 import org.springframework.stereotype.Service;
 import com.minibank.repository.ExtratoRepository;
+import java.time.LocalDate;
 
 @Service //logica/regras da aplicacao
 public class ExtratoService {
 
- private final ExtratoRepository extratoRepository;
+ private final ExtratoRepository extratoRepository; //atributo que guarda o repósitory
 
+
+
+//construtor:
   public ExtratoService(ExtratoRepository extratoRepository) {
       this.extratoRepository = extratoRepository;
+      //atributo - parametro
+  }
+
+
+  //METODO QUE REGISTRA A MOVIMENTACAO
+    //recebe objeto dto contendo os dados enviados pelo front
+  public void registrar(RegistrarExtratoRequestDTO request){
+      Extrato extrato = new Extrato();
+      extrato.setCriancaId(request.getCriancaId());
+      //Pegamos o criancaId que está dentro do DTO e colocamos dentro do objeto Extrato.
+      extrato.setTipo(request.getTipo()); //(pegamos o tipo entrada/retirada)
+      extrato.setValor(request.getValor()); //pega o valor enviado pelo front
+      extrato.setDescricao(request.getDescricao()); //
+      extrato.setData(LocalDate.now());
+
+      // O Repository é quem vai executar o INSERT no MySQL.
+      extratoRepository.salvar(extrato);
   }
 }
